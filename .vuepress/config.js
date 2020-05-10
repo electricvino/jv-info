4d1b95c97c9e946f1007bbe9df04e429740d642c
Jon Vinson <jon.vinson@gmail.com>
2020-05-09 22:19:11 -0500

    add blog plugin

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -12,6 +12,32 @@ module.exports = {
         ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
         ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     ],
+    plugins: [
+        '@vuepress/blog',
+        {
+            id: 'post',
+            // Target directory
+            dirname: '_posts',
+            // Path of the `entry page` (or `list page`)
+            path: '/post/',
+            layout: 'MyIndexPost',
+            itemLayout: 'MyPost',
+            frontmatters: [
+                {
+                  // Unique ID of current classification
+                  id: 'tag',
+                  // Decide that the frontmatter keys will be grouped under this classification
+                  keys: ['tag'],
+                  // Path of the `entry page` (or `list page`)
+                  path: '/tag/',
+                  // Layout of the `entry page`
+                  layout: 'Tags',
+                  // Layout of the `scope page`
+                  scopeLayout: 'Tag'
+                },
+              ],
+        },
+      ],
     serviceWorker: true,
     themeConfig: {
         docsDir: 'blog',
